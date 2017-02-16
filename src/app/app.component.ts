002f4822cd9c968036782892ceeaa1beabb2d1a7
Cedric Leroy <cedie73@hotmail.fr>
2017-02-16 06:43:21 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+import { IMyOptions, IMyDateRange, IMyDateRangeModel } from 'mydaterangepicker';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  private dateRangePickerOptions: IMyOptions = {
+       dateFormat: 'dd.mm.yyyy',
+  };
+
+  selectedDateRangeNormal:IMyDateRange = undefined; // null does not work
+
+  onDateRangeChanged(event: IMyDateRangeModel){
+     console.log(event);
+     console.log(this.selectedDateRangeNormal);
+
+     // adding the below, init with null is working
+     /*if(event.formatted !== '') {
+       this.selectedDateRangeNormal = {beginDate: event.beginDate, endDate: event.endDate};
+     }*/
+  }
+
+  clearDateRange(){
+    console.log('clear');
+    this.selectedDateRangeNormal = null;
+  }
+
 }
